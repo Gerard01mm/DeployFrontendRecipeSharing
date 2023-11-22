08890d28af0236af10c9679f4bef36969da4c284
Gerard01mm <56315258+Gerard01mm@users.noreply.github.com>
2023-11-22 23:30:13 +0100

    First Commit

---
+++ src/router.js
@@ -0,0 +1,48 @@
+import {createWebHistory, createRouter} from "vue-router";
+
+import HomePage from "./pages/HomePage.vue";
+import RegisterPage from "./pages/RegisterPage.vue";
+import LoginPage from "./pages/LoginPage.vue";
+import AddRecipePage from "./pages/AddRecipePage.vue";
+import ProfilePage from "./pages/ProfilePage.vue"
+import RecipePage from "./pages/RecipePage.vue"
+
+const routes = [
+  {
+    path: "",
+    component: HomePage,
+    name: "home",
+  },
+  {
+    path: "/register",
+    component: RegisterPage,
+    name: "register",
+  },
+  {
+    path: "/login",
+    component: LoginPage,
+    name: "login",
+  },
+  {
+    path: "/addRecipe",
+    component: AddRecipePage,
+    name: "addRecipe",
+  },
+  {
+    path: "/profile",
+    component: ProfilePage,
+    name: "profile",
+  },
+  {
+    path: "/recipePage",
+    component: RecipePage,
+    name: "recipePage",
+  },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: routes,
+});
+
+export default router;
